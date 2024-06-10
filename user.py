573d6de1c2e58d4c5ba5bfd6c5c459d6025bb0fa
Arief Badal <ariefbadal21@outlook.com>
2024-06-10 16:22:13 +0200

    Add ability to iterate over objects in Profile class

--- user.py
+++ user.py
@@ -9,14 +9,20 @@ class User:
         
     
 class Profile(User):
-    def __init__(self, id, fullname,role_id,email, username, password, mobile, age, gender,weight):
-        super().__init__(id, fullname, role_id, email,username, password)
-        self.mobile=mobile
+    def __init__(self, id, fullname, role_id, email, username, password, mobile, age, gender, weight):
+        super().__init__(id, fullname, role_id, email, username, password)
+        self.mobile = mobile
         self.age = age
         self.gender = gender
-        self.weight =weight
+        self.weight = weight
+
+    def __iter__(self):
+        attributes = ['id', 'fullname', 'role_id', 'email', 'username', 'password', 'mobile', 'age', 'gender', 'weight']
+        for attr in attributes:
+            yield attr, getattr(self, attr)
+
     def __str__(self) -> str:
-        return f"Name: {self.fullname} Role: {self.role_id}, Email: {self.email} username:{self.username}"
+        return f"Name: {self.fullname}, Role: {self.get_role()}, Email: {self.email}, Username: {self.username}"
 
     def get_role(self):
         if self.role_id == 1:
@@ -24,6 +30,8 @@ class Profile(User):
         elif self.role_id == 2:
             return "admin"
         elif self.role_id == 3:
+            return "consultant"
+        elif self.role_id == 4:
             return "member"
         else:
             return "Invalid role"
